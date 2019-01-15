4ab171fafa00470982c6d9a0a6ec4a9c0bdbf194
杨成光 <1213215489@qq.com>
2019-01-15 19:23:13 +0800

    基本程序框架

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
+int main()
+ {   
+    printf("minganzifuguanlixitong\n");
+    {
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu\n");
+        printf("3---chakansuoyouminganzifu\n");
+        printf("4---tihuannierongzhongdeminganzifu\n");
+        printf("5---tuichu\n");
 
-int main(){
+        printf("qingxuanzexiangyinggongneng\n");
 
-    printf("hello world!");
+        int code;
+        scanf("%d",&code);
 
-    return 0;
+        if(code == 1){
+
+        }
+        if(code == 2){
+
+        }
+         if(code == 3){
+
+        }
+         if(code == 4){
+
+        }
+         if(code == 5){
+             printf("tuichuxitong\n");
+        }
+              
+    }
+
+             return 0;
+
 
 }
